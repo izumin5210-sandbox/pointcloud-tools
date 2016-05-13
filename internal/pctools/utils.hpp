d5f3d79805d853c116f69a545d695642b9d910d8
izumin5210 <masayuki@izumin.info>
2016-05-13 22:18:38 +0900

    Downsample with voxel filter util

--- internal/pctools/utils.hpp
+++ internal/pctools/utils.hpp
@@ -3,6 +3,7 @@
 //
 #include <pcl/console/parse.h>
 #include <pcl/console/print.h>
+#include <pcl/filters/voxel_grid.h>
 
 #include <boost/filesystem.hpp>
 #include <boost/range/iterator_range.hpp>
@@ -24,6 +25,16 @@ void each_files(std::string path, std::function<void(bfs::directory_entry)> proc
   for (auto file : boost::make_iterator_range(bfs::directory_iterator(path), bfs::directory_iterator())) {
     proc(file);
   }
+}
+
+template <typename T>
+void downsample(typename pcl::PointCloud<T> &cloud_in, typename pcl::PointCloud<T> &cloud_out, float voxelSize) {
+  pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_tmp(new pcl::PointCloud<pcl::PointXYZRGBA>);
+  pcl::copyPointCloud(cloud_in, *cloud_tmp);
+  pcl::VoxelGrid<T> sor;
+  sor.setInputCloud(cloud_tmp);
+  sor.setLeafSize(voxelSize, voxelSize, voxelSize);
+  sor.filter(cloud_out);
 }
 }
 
